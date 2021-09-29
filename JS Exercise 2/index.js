1516ae5de7e065e6794b3b09c6432d54f046a21b
Secrieru123 <82930216+Secrieru123@users.noreply.github.com>
2021-09-29 11:55:35 +0300

    Dog Years

---
+++ JS Exercise 2/index.js
@@ -0,0 +1,20 @@
+const myAge = 25 // In this variable const I wrote my age as value and I set this const variable because this variable is not going to change it’s value  
+
+let earlyYears = 2 // In this variable i set value 2 :) !
+
+earlyYears = earlyYears * 10.5
+
+let laterYears = myAge - 2 // I subtracted 2 from myAge
+
+laterYears = laterYears * 4 // I multiplicate the laterYears variable by 4
+console.log(earlyYears,laterYears); // result  21  92
+
+myAgeInDogYears = earlyYears + laterYears // I add the earlyYears with laterYears and i received my age in dog year 
+console.log(myAgeInDogYears);
+
+
+myName = 'Ion'
+myName = myName.toLowerCase() // I wrote my name to lowercase letters
+
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
