c67f8c7168226e3df57a2f8cffc43fd6d784a5ea
Secrieru Ion <secrieru123@gmail.com>
2021-11-07 22:23:52 +0200

    button/class

---
+++ APP/Button/script.js
@@ -0,0 +1,28 @@
+class Button {
+    constructor(root, text, type, action) {
+        this.root = root
+        this.text = text
+        this.type = type
+        this.action = action
+        this.render()
+    }
+
+    render(root) {
+        
+        this.root.innerHTML += `<button class="${this.type}" onclick="${this.action}()">${this.text}</button>`  
+    }
+   
+}
+
+function okAction() {
+   
+    alert("You've accepted")
+}
+
+function cancelAction() {
+    alert("You've canceled")
+}
+
+let okButton = new Button (window["actions"], "OK", "success", "okAction")
+
+let cancelButton = new Button (window["actions"], "CANCEL", "danger", "cancelAction")
