e9a3baf2b6698fafbc72f6867412602ceaf2b6b1
Secrieru Ion <secrieru123@gmail.com>
2021-11-10 21:01:25 +0200

    proloader

--- APP/ProgressPreloader/proloader.js
+++ APP/ProgressPreloader/proloader.js
@@ -1,116 +1,153 @@
-// progress like preloader component
+
+
+// common preloader logic
 class BasePreloader {
 
     constructor(rootDiv) {
-       this.rootDiv = rootDiv
-        this.init()
-        // class specific logic <<<<<<
+        this.rootDiv = rootDiv
 
+        // HW4.
+        this.init()
         this.onload()
     }
+        
 
     onload() {
-        this.timerId = setInterval(()=>{
+        this.timerID = setInterval(()=>{
             this.step()
-            if (this.condition()) {
-                
-                clearInterval(this.timerId)
+            if(this.condition()) {
+    
+                clearInterval(this.timerID)
             }
             this.rootDiv.innerHTML = this.render()
-        },500)
+        },this.repeat) 
     }
     
-    condition() {}
-    step() {}   
-    render() {}
+
+    // abstract meth ods
     init() {}
 
+    //HW7.
+    condition() {
+        throw new ReferenceError(`Condition is check as: ${this.check()}`)
+    }
+    check() {}
+    step() {}
+    render() {}
 }
 
 
-class ProgressPreloader extends BasePreloader{
+class ProgressPreloader extends BasePreloader {
 
     constructor(rootDiv) {
         super(rootDiv)
+        this.progress = 0  // 0....100 %
     }
-
-/*     onload() {
-        this.timerId = setInterval(()=>{
-            this.progress += 10
-            if(this.progress >= 100) {
-                this.progress = 100
-            }
-            this.rootDiv.innerHTML = `[ ${this.progress}% ]`
-        },500)
-    } */
-
-    // overriding methods 
 
     init() {
-        this.progress = 0
+        this.repeat = 500
     }
 
+
+    // Overriding methods
     condition() {
         return this.progress >= 100
     }
+
     step() {
         this.progress += 10
     }
-    render() {
-        return `[${this.progress}%]`
+
+    render () {
+        return `[ ${this.progress}% ]`
     }
+
+
 }
 
 
-
-class CircularPreloader extends BasePreloader{
+class CircularPreloader extends BasePreloader {
 
     constructor(rootDiv) {
-        this.rootDiv = rootDiv
-    
-        this.onload()
+        super(rootDiv)
+        this.duration = 3000
+        this.frames = ['|', '/', '--', '\\'] ///// JS String Character Escaping (caractere speciale in string JS)
     }
 
-    /* onload() {
-        this.timerId = setInterval(()=>{
-            this.progress -= 500
-            if(this.progress >= 100) {
-                clearInterval(this.timerId)
-            }
+    init() {
+        this.repeat = 250
+    }
+   
 
-            // round sht frame = this.frames.shift()
-            this.frame.push(frame)
-            console.log(this.frames)
-            this.rootDiv.innerHTML = `[ ${frame}% ]`
-        },250)
+    condition() {
+        return this.progress <= 0
+    }
+
+
+    // HW2.
+    
+    /* step() {
+        this.duration -= 250
+        let frame = this.frames.shift()
+        this.frames.push(frame)  
     } */
 
 
-    init() {
-        this.progress = 3000
-        this.frames = ['|', '/', '--', '\\']   // JS String Character Escaping
-    }
-    condition() {
-        return this.duration <= 0
-    }
+    // HW.3
     step() {
-            this.duration -= 250
-       let frame = this.frames.shift()
-            this.frame.push(frame)
-    }
-    render() {
-        return `[ ${this.frame[0]}]`
+        this.duration -= 250
+           let frame = this.frames.splice(0,0,this.frames[3])
+           frame = this.frames.splice(3,0)    
     }
 
+
+/*     step() {
+        this.duration -= 250
+        let frame = this.frames.pop()
+        this.frames.unshift(frame)
+    } */
+
+    render () {
+        return   `[ ${this.frames[0]} ]`
+    }
 }
+    // HW5.
+ class IncompletePreloader extends BasePreloader {
+
+    constructor(rootDiv) {
+        super(rootDiv)
+        this.progress = 0  // 0....100 %
+    }
+
+    init() {
+        this.repeat = 500
+    }
+
+
+    step() {
+        this.progress += 10
+    }
+
+    render () {
+        return `[ ${this.progress}% ]`
+    }
+
+    }
 
 
 
-
-
-///////////////////////////////
+////////////////////////////////////////////////////
 
 let pp1 = new ProgressPreloader(window["prel-1"])
-let pp2 = new CircularPreloader(windows["pre2-2"])
-console.log(pp1);
-console.log(pp2);
+let pp2 = new CircularPreloader(window["prel-2"])
+
+   // HW6. 
+let pp3 = new IncompletePreloader(window["prel-3"])
+console.log(pp3)
+    /* In interiorul clasei IncompletePreloader lipseste metoda condition(),
+    astfel "if" va returna mereu "false" si codul din interiorul if-ului
+    nu se va executa niciodata. Atunci "this.pprogress" va creste pana la infinit. */
+
+
+
+
